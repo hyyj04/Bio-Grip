4d1d3444478abbd0f658d14e9d682b97f1e9e343
hyyj04 <hyyj56@gmail.com>
2026-04-01 04:27:41 +0900

    GSR Raw Data update & code update

--- GSR/get_GSR_RawData/src/main.cpp
+++ GSR/get_GSR_RawData/src/main.cpp
@@ -5,7 +5,7 @@ const float ADC_REF = 3.3f;
 const int ADC_MAX = 1023;
 
 const unsigned long SAMPLE_INTERVAL_MS = 100;
-const unsigned long DURATION_MS = 60000;  // 60초
+const unsigned long DURATION_MS = 300000;  // 300초 5분
 
 unsigned long lastSampleTime = 0;
 unsigned long startTime = 0;
@@ -28,7 +28,7 @@ void loop() {
 
   unsigned long now = millis();
 
-  // 60초 지나면 종료
+  // 300초 지나면 종료
   if (now - startTime >= DURATION_MS) {
     isRunning = false;
     Serial.println("END");
